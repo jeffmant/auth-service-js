a2cec847297cb7d24a6b272882b8243537a0f892
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-02-21 16:36:44 -0300

    feat: ensure SigninRouter returns 400 if an invalid email is provided

--- src/presentation/routers/signin.router.js
+++ src/presentation/routers/signin.router.js
@@ -1,16 +1,23 @@
+import { InvalidParamError } from '../errors/invalid-param.error'
 import { MissingParamError } from '../errors/missing-param.error'
 import { HttpResponse } from '../helpers/http.helper'
 
 export class SigninRouter {
-  constructor (authUseCase) {
+  constructor (authUseCase, emailValidator) {
     this.authUseCase = authUseCase
+    this.emailValidator = emailValidator
   }
 
   async route (httpRequest) {
     try {
       const { email, password } = httpRequest.body
+
       if (!email) {
         return HttpResponse.badRequest(new MissingParamError('email'))
+      }
+
+      if (!this.emailValidator.isValid('email')) {
+        return HttpResponse.badRequest(new InvalidParamError('email'))
       }
 
       if (!password) {
