97866bac50fe29c573c91638f92393014da05135
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-02-21 13:45:12 -0300

    feat: ensure if signin route returns 500 if don't exist httpRequest or it hasn't body

--- src/presentation/router/signin.router.js
+++ src/presentation/router/signin.router.js
@@ -1,5 +1,11 @@
 export default class SigninRouter {
   route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+
     const { email, password } = httpRequest.body
     if (!email || !password) {
       return {
