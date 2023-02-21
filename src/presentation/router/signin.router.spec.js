851f434b550966721f6efeec060ec30be863d683
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-02-21 13:39:52 -0300

    test: ensure signin router returns 400 if request body contains no password

--- src/presentation/router/signin.router.spec.js
+++ src/presentation/router/signin.router.spec.js
@@ -5,8 +5,18 @@ describe('Signin Router', () => {
     const sut = new SigninRouter()
     const httpRequest = {
       body: {
-        email: 'any@email.com',
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SigninRouter()
+    const httpRequest = {
+      body: {
+        email: 'any@email.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
