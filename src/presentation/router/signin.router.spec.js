51de77ccc29977eea50fcbfbb4c5cb7b5f7a6008
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-02-21 13:45:47 -0300

    test: ensure if signin route returns 500 if don't exist httpRequest or it hasn't body

--- src/presentation/router/signin.router.spec.js
+++ src/presentation/router/signin.router.spec.js
@@ -22,4 +22,17 @@ describe('Signin Router', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
+
+  test('Should return 500 if no httpRequest is provided', () => {
+    const sut = new SigninRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body', () => {
+    const sut = new SigninRouter()
+    const httpRequest = {}
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
 })
