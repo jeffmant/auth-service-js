db079027148f44e2d49e5ec3853daefc4c939478
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-02-24 15:06:10 -0300

    test: move sut creation to a factory helper method

--- tests/utils/email-validator.spec.js
+++ tests/utils/email-validator.spec.js
@@ -1,16 +1,18 @@
 import { EmailValidator } from '../../src/utils/email-validator'
 import validator from 'validator'
 
+const makeSut = () => new EmailValidator()
+
 describe('Email Validator', () => {
   test('Should return true if validator returns true', async () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isValid = await sut.isValid('valid_email@email.com')
     expect(isValid).toBe(true)
   })
 
   test('Should return false if validator returns false', async () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isValid = await sut.isValid('invalid_email')
     expect(isValid).toBe(false)
   })
